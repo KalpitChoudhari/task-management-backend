45b2fa160957df21586da0ca88b31f1a9bfe94b3
KalpitChoudhari <choudharikalpit120@gmail.com>
2024-02-25 01:33:19 +0530

    feat: added create, update & destroy tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,6 +1,33 @@
 class Api::V1::TasksController < ApplicationController
   before_action :authenticate_user!
 
+  def create
+    task = current_user.tasks.build(title: params[:title], description: params[:description])
+    if task.save
+      render json: task, status: :ok
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    task = current_user.tasks.find(params[:id])
+    if task.update(status: params[:status])
+      render json: task, status: :ok
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    task = current_user.tasks.find(params[:id])
+    if task.destroy
+      render json: task, status: :ok
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def index
     tasks = current_user.tasks
     render json: tasks, status: :ok
