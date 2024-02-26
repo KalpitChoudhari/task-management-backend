5d17662d7e6b1bb8b833d2ff1907cdfa3f8ae5f5
KalpitChoudhari <choudharikalpit120@gmail.com>
2024-02-26 20:51:52 +0530

    feat: added task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
+  validates :status, presence: true, inclusion: { in: %w(todo in_progress completed) }
 
   enum status: { todo: 0, in_progress: 1, completed: 2 }
 end
